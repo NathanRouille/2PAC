b2e0e4db846f345b094f84003fdbdf08f3f629de
Titouan Duhaze <titouan.duhaze@telecom-paris.fr>
2024-06-25 20:09:36 +0200

    modification de data_struct.py

--- 2pac/data_struct.py
+++ 2pac/data_struct.py
@@ -13,7 +13,27 @@ class Block:
     def __init__(self, sender):
         self.sender = sender
 
+class Block1(Block):
+    def __init__(self, sender, block):
+        super().__init__(sender)
+        self.block = block
+
+class Block2(Block):
+    def __init__(self, sender, block, qc):
+        super().__init__(sender)
+        self.block = block
+        self.qc = qc
+
 class Vote:
-    def __init__(self, vote_sender, block_sender):
+    def __init__(self, vote_sender):
         self.vote_sender = vote_sender
+
+class Vote1(Vote):
+    def __init__(self, vote_sender, block_sender):
+        super().__init__(vote_sender)
         self.block_sender = block_sender
+
+class Vote2(Vote):
+    def __init__(self, vote_sender, qc_sender):
+        super().__init__(vote_sender)
+        self.qc_sender = qc_sender
