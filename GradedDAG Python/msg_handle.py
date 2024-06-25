ced9e673b9fff5e48237e0ba6dcfa28bc21ebe7a
Rayane Dakhlaoui <rayane.dakhlaoui@telecom-paris.fr>
2024-06-25 13:27:45 +0200

    Ajout de msg_handle.py

---
+++ GradedDAG Python/msg_handle.py
@@ -0,0 +1,108 @@
+import asyncio
+from asyncio import Queue
+from threading import Lock
+
+class Node:
+    def __init__(self, trans, cbc, logger, name):
+        self.trans = trans
+        self.cbc = cbc
+        self.logger = logger
+        self.name = name
+        self.is_faulty = False
+        self.lock = Lock()
+    
+    async def handle_msg_loop(self):
+        msg_ch = self.trans.msg_chan()
+        while True:
+            msg_with_sig = await msg_ch.get()
+            if self.is_faulty:
+                continue
+            
+            msg_asserted = msg_with_sig['msg']
+            if isinstance(msg_asserted, Block):
+                if not self.verify_sig_ed25519(msg_asserted.sender, msg_with_sig['msg'], msg_with_sig['sig']):
+                    self.logger.error("fail to verify the block's signature", round=msg_asserted.round, sender=msg_asserted.sender)
+                    continue
+                asyncio.create_task(self.cbc.handle_block_msg(msg_asserted))
+            
+            elif isinstance(msg_asserted, Elect):
+                if not self.verify_sig_ed25519(msg_asserted.sender, msg_with_sig['msg'], msg_with_sig['sig']):
+                    self.logger.error("fail to verify the echo's signature", round=msg_asserted.round, sender=msg_asserted.sender)
+                    continue
+                asyncio.create_task(self.handle_elect_msg(msg_asserted))
+            
+            elif isinstance(msg_asserted, Ready):
+                if not self.verify_sig_ed25519(msg_asserted.ready_sender, msg_with_sig['msg'], msg_with_sig['sig']):
+                    self.logger.error("fail to verify the ready's signature", round=msg_asserted.round, sender=msg_asserted.ready_sender, block_sender=msg_asserted.block_sender)
+                    continue
+                asyncio.create_task(self.cbc.handle_ready_msg(msg_asserted))
+            
+            elif isinstance(msg_asserted, Done):
+                if not self.verify_sig_ed25519(msg_asserted.done_sender, msg_with_sig['msg'], msg_with_sig['sig']):
+                    self.logger.error("fail to verify the done's signature", round=msg_asserted.round, sender=msg_asserted.done_sender, block_sender=msg_asserted.block_sender)
+                    continue
+                asyncio.create_task(self.handle_done_msg(msg_asserted))
+            
+            elif isinstance(msg_asserted, Vote):
+                if not self.verify_sig_ed25519(msg_asserted.vote_sender, msg_with_sig['msg'], msg_with_sig['sig']):
+                    self.logger.error("fail to verify the vote's signature", round=msg_asserted.round, sender=msg_asserted.vote_sender, block_sender=msg_asserted.block_sender)
+                    continue
+                asyncio.create_task(self.cbc.handle_vote_msg(msg_asserted))
+    
+    async def handle_cbc_block(self, block):
+        asyncio.create_task(self.try_to_update_dag(block))
+    
+    async def handle_elect_msg(self, elect):
+        with self.lock:
+            self.store_elect_msg(elect)
+            self.try_to_elect_leader(elect.round)
+    
+    async def handle_done_msg(self, done):
+        with self.lock:
+            self.store_done(done)
+        asyncio.create_task(self.try_to_next_round(done.round))
+        self.try_to_commit_leader(done.round)
+    
+    async def cbc_output_block_loop(self):
+        data_ch = self.cbc.return_block_chan()
+        while True:
+            block = await data_ch.get()
+            self.logger.debug("Block is received by from CBC", node=self.name, round=block.round, proposer=block.sender)
+            asyncio.create_task(self.handle_cbc_block(block))
+    
+    async def done_output_loop(self):
+        data_ch = self.cbc.return_done_chan()
+        while True:
+            done = await data_ch.get()
+            asyncio.create_task(self.handle_done_msg(done))
+            # make sure every node can get 2f+1 done
+            # asyncio.create_task(self.broadcast_done(done))
+
+# Example classes for Block, Elect, Ready, Done, and Vote
+class Block:
+    def __init__(self, sender, round):
+        self.sender = sender
+        self.round = round
+
+class Elect:
+    def __init__(self, sender, round):
+        self.sender = sender
+        self.round = round
+
+class Ready:
+    def __init__(self, ready_sender, block_sender, round):
+        self.ready_sender = ready_sender
+        self.block_sender = block_sender
+        self.round = round
+
+class Done:
+    def __init__(self, done_sender, block_sender, round):
+        self.done_sender = done_sender
+        self.block_sender = block_sender
+        self.round = round
+
+class Vote:
+    def __init__(self, vote_sender, block_sender, round):
+        self.vote_sender = vote_sender
+        self.block_sender = block_sender
+        self.round = round
