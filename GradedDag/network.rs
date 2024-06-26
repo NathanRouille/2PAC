2cd4e540b0ae349b4aba22b671fd1c01df492d19
Adam Chgour <adam.chgour@telecom-paris.fr>
2024-06-26 10:51:00 +0200

    commentaires network.rs

--- GradedDag/network.rs
+++ GradedDag/network.rs
@@ -10,6 +10,25 @@ pub struct Transport {
 }
 
 impl Node {
+    /// Démarre l'écoute P2P sur le port spécifié dans le cluster.
+    /// Cette fonction lie le listener TCP à l'adresse IP "0.0.0.0" et au port spécifié.
+    /// Elle configure également le listener en mode non bloquant.
+    /// Enfin, elle crée une instance de la structure `Transport` et la stocke dans le champ `trans` de `Node`.
+    ///
+    /// # Erreurs
+    ///
+    /// Cette fonction peut renvoyer une erreur si le port spécifié n'est pas trouvé dans le cluster.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// # use std::io;
+    /// # use std::net::TcpListener;
+    /// # use std::sync::{Arc, Mutex};
+    /// # use crate::node::{Node, Transport};
+    /// let mut node = Node::new();
+    /// node.StartP2PListen().unwrap();
+    /// ```
     pub fn StartP2PListen(&mut self) -> io::Result<()> {
         let port = *self.cluster_port.get(&self.name).ok_or(io::Error::new(io::ErrorKind::Other, "Port not found"))?;
         let addr = format!("0.0.0.0:{}", port);
@@ -24,6 +43,26 @@ impl Node {
         Ok(())
     }
     
+    /// Établit des connexions P2P avec tous les nœuds du cluster.
+    /// Cette fonction itère sur tous les adresses IP et ports du cluster et établit une connexion TCP avec chaque nœud.
+    /// Elle configure également un délai de lecture de 30 secondes pour chaque connexion.
+    /// Enfin, elle stocke chaque connexion dans le champ `connections` de la structure `Transport`.
+    ///
+    /// # Erreurs
+    ///
+    /// Cette fonction peut renvoyer une erreur si `networkTransport` n'a pas été créé.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// # use std::io;
+    /// # use std::net::TcpStream;
+    /// # use std::sync::{Arc, Mutex};
+    /// # use std::time::Duration;
+    /// # use crate::node::{Node, Transport};
+    /// let mut node = Node::new();
+    /// node.EstablishP2PConns().unwrap();
+    /// ```
     pub fn EstablishP2PConns(&mut self) -> io::Result<()> {
         let trans = self.trans.as_ref().ok_or(io::Error::new(io::ErrorKind::Other, "networkTransport has not been created"))?.clone();
         
